0ad4d1006c03de6f1dbd8273621804df30bdc67d
Valeriia Isaienko <93624578+Magic1vy@users.noreply.github.com>
2023-05-13 00:46:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ search.addEventListener("keyup", function(event){
     const word = event.target.value.toLowerCase()
 
     divs.forEach( item =>{
-        item.querySelector("p").textContent.toLowerCase().includes(word) ? (item.style.display = "block") :  (item.style.display = "none")
+        item.querySelector("a").textContent.toLowerCase().includes(word) ? (item.style.display = "block") :  (item.style.display = "none")
     })
 })
